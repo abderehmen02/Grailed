ee02b5ef0f0237f1f71e79455bb917d31a3b0ef8
abderrahmane <ilahalaha0987@gmail.com>
2024-01-25 14:39:50 +0100

    initialazing app

---
+++ src/components/layout/headers.tsx
@@ -0,0 +1,12 @@
+export const Header  = ()=>{
+    return <div>
+        <video poster="https://assets.grailed.com/logo.jpg" className="Logo-module__image___3LWEN"  autoPlay playsInline loop ><source src="https://assets.grailed.com/logo.webm" type="video/webm"/><source src="https://assets.grailed.com/logo.mp4" type="video/mp4"/>
+        </video>
+        <div className="flex" >
+        <input/>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+}
