a119a6d0315d543ee219734c0d71330ba0b4013a
abderrahmane <ilahalaha0987@gmail.com>
2024-01-25 16:47:22 +0100

    fixing the video border issue

--- src/components/layout/headers.tsx
+++ src/components/layout/headers.tsx
@@ -5,12 +5,13 @@ import { PrimaryButton } from "../ui/buttons"
 export const Header  = ()=>{
     return <div className="flex items-center justify-center py-4 gap-8" >
     <div className="flex gap-4" >
-        <video src="/logo.mp4" poster="/logo.jpg" className="max-w-[13rem]"  autoPlay={true} playsInline={true} loop={true} >
+        <video  poster="/logo.jpg" className="max-w-[13rem] border-2 b outline-none border-none"  autoPlay={true} playsInline={true} loop={true} >
             <source src="/logo.webm" type="video/webm"/>
             <source src="/logo.mp4" type="video/mp4"/>
         </video>
-        <div className="flex" >
-        <textarea/>
+        <div className="flex px-4 gap-5 centerContent  border-2 border-black" >
+        <i style={{WebkitTextStroke : "1px"}} className="bi bi-search   h-fit  font-bold   "></i>
+        <input className="w-[400px] outline-none h-fit "  />
         </div>
     </div>
         <div>
