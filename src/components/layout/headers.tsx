73065a2a1dd79cd2067fd962677c0527410fa9fb
abderrahmane <ilahalaha0987@gmail.com>
2024-01-25 17:03:06 +0100

    styling the primary button

--- src/components/layout/headers.tsx
+++ src/components/layout/headers.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { PrimaryButton } from "../ui/buttons"
 
 export const Header  = ()=>{
-    return <div className="flex items-center justify-center py-4 gap-8" >
+    return <div className="flex items-center justify-center py-4 gap-40" >
     <div className="flex gap-4" >
         <video  poster="/logo.jpg" className="max-w-[13rem] border-2 b outline-none border-none"  autoPlay={true} playsInline={true} loop={true} >
             <source src="/logo.webm" type="video/webm"/>
@@ -14,10 +14,10 @@ export const Header  = ()=>{
         <input className="w-[400px] outline-none h-fit "  />
         </div>
     </div>
-        <div>
+        <div className="flex gap-6" >
             <PrimaryButton>SELL</PrimaryButton>
-            <Link href={appConfig.routes.shop} >SOP</Link>
-            <Link href={appConfig.routes.drycleanonly} >READ</Link>
+            <Link href={appConfig.routes.shop} ><PrimaryButton className="border-none" >SHOP</PrimaryButton></Link>
+            <Link href={appConfig.routes.drycleanonly} ><PrimaryButton className="border-none" >READ</PrimaryButton></Link>
             <PrimaryButton>LOGIN</PrimaryButton>
             <PrimaryButton>SITN UP</PrimaryButton>
         </div>
--- src/components/ui/buttons.tsx
+++ src/components/ui/buttons.tsx
@@ -1,9 +1,10 @@
+import { cn } from "@/lib/tailwind";
 import { HTMLAttributes } from "react";
 
 
 
 export const PrimaryButton : React.FC<HTMLAttributes<HTMLButtonElement>> = ({ className , children ,  ...btnProps})=>{
-return <button className={className} {...btnProps}  >
+return <button className={cn( "px-4 py-2 text-sm border-black border-2" ,className)} {...btnProps}  >
 {children}
 </button>
 }
