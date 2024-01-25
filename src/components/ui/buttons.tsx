3b4f81c8d4f93e0fc1ba7f48d58185fb833233f6
abderrahmane <ilahalaha0987@gmail.com>
2024-01-25 14:59:19 +0100

    integrating the primary btn

--- src/components/ui/buttons.tsx
+++ src/components/ui/buttons.tsx
@@ -2,8 +2,8 @@ import { HTMLAttributes } from "react";
 
 
 
-export const PrimaryButton : React.FC<HTMLAttributes<HTMLButtonElement>> = ({ className ,  ...btnProps})=>{
+export const PrimaryButton : React.FC<HTMLAttributes<HTMLButtonElement>> = ({ className , children ,  ...btnProps})=>{
 return <button className={className} {...btnProps}  >
-
+{children}
 </button>
 }
