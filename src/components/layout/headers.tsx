5f80a2fc8c539e88761105d88a987039f8ae70b0
abderrahmane <ilahalaha0987@gmail.com>
2024-01-26 20:01:49 +0100

    intgrating the subheader in the header

--- src/components/layout/headers.tsx
+++ src/components/layout/headers.tsx
@@ -3,7 +3,8 @@ import Link from "next/link"
 import { PrimaryButton, SecondaryButton, TextButton } from "../ui/buttons"
 
 export const Header  = ()=>{
-    return <div className="flex border-whinGray border-b-2 items-center justify-center py-6 gap-40" >
+    return <div>
+    <div className="flex border-whinGray border-b-2 items-center justify-center py-6 gap-40" >
     <div className="flex gap-4" >
         <video  poster="/logo.jpg" className="max-w-[13rem] border-2 b outline-none border-none"  autoPlay={true} playsInline={true} loop={true} >
             <source src="/logo.webm" type="video/webm"/>
@@ -22,6 +23,18 @@ export const Header  = ()=>{
             <TextButton className="border-stone-500 font-semibold" >LOGIN</TextButton>
             <SecondaryButton  >SITN UP</SecondaryButton>
         </div>
+    </div>
+<div className="flex w-full items-center py-2 justify-center border-whinGray border-b-2" >
+<div className="flex pageMaxWidth w-full justify-between items-center " >
+<div className="text-sm font-semibold">DESIGNERS</div>
+<div className="text-sm font-semibold">MENSWEAR</div>
+
+<div className="text-sm font-semibold">WOMENSWEAR</div>
+<div className="text-sm font-semibold">SNEAKERS</div>
+<div className="text-sm font-semibold">STAFF PICKS</div>
+<div className="text-sm font-semibold">COLLECTIONS</div>
+</div>
+</div>
     </div>
 }
 
