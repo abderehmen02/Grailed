c42f91ff27fb25593f0041e06b1ec0dd6bd1f264
abderrahmane <ilahalaha0987@gmail.com>
2024-01-26 20:15:23 +0100

    integrating the hero video

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main >
-      my app 
+    <main>
+      <div className="hero w-full h-[500px] relative" >
+        <video className="w-full object-cover h-full"  poster="/heroVideoPoster.jpg" src="/heroVideo.mp4" autoPlay={true} playsInline={true} loop={true} ></video>
+      </div>
     </main>
   );
 }
