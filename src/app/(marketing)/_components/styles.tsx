5119dff3f007d18ddbee30b2ff1b1341007d8a0a
abderrahmane <ilahalaha0987@gmail.com>
2024-01-27 10:34:28 +0100

    fixing deployement issues

--- src/app/(marketing)/_components/styles.tsx
+++ src/app/(marketing)/_components/styles.tsx
@@ -10,7 +10,7 @@ export const Styles : React.FC<{styles : ProductStyle[]}> =  ({styles})=>{
     return <div className="flex gap-6 py-20  flex-col" >
    <h3 className="text-2xl font-bold " >Shop by Style</h3>        
 <div className="flex items-center gap-8" >        {
-            styles.map(style=><HomeStyleCard {...style} />)
+            styles.map(style=><HomeStyleCard key={style.name} {...style} />)
         }</div>
     </div>
 }
--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -11,7 +11,7 @@ const styles = await fetchStyles()
         <video className="w-full absolute top-0 left-0 z-0 object-cover h-full"  poster="/heroVideoPoster.jpg" src="/heroVideo.mp4" autoPlay={true} playsInline={true} loop={true} ></video>
         <div className="flex flex-col gap-6 items-center justify-center text-white z-10" >
           <h3 className="text-4xl font-bold text-center" >THE PLATFORM FOR PERSONAL STYLE</h3>
-          <h4 className="text-3xl max-w-[600px] text-center" >Buy, sell, discover authenticated pieces from the world's top brands.</h4>
+          <h4 className="text-3xl max-w-[600px] text-center" >Buy, sell, discover authenticated pieces from the world&rsquo;s top brands.</h4>
           <div className="flex  gap-11" >
           <button className="p-4 border-2 border-white text-xl font-bold" >SHOP MENSWEAR</button>
           <button className="p-4 border-2 border-white text-xl font-bold" >SHOP WOMENSWEAR</button>
