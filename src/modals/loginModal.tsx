d4116f9e8038532329ab54cc3b7deb69f7db399a
abderrahmane <ilahalaha0987@gmail.com>
2024-01-28 22:37:57 +0100

    optimizing the sign in modal

--- src/modals/loginModal.tsx
+++ src/modals/loginModal.tsx
@@ -4,23 +4,32 @@ import { Modal } from "@mui/material";
 import { useLoginModal } from "@/store/loginModal";
 import {Box}from "@mui/material";
 import { AppleSignInButton, FacebookSignInButton, GoogleSignInButton } from "@/components/auth/components";
+import { SecondaryButton } from "@/components/ui/buttons";
+import { useSignUpModal } from "@/store/signUpModel";
 
 
 export default function LoginModal() {
     const { isOpen , close , open}= useLoginModal()
+    const {open : openSignUpModal} = useSignUpModal()
   return (
     <Modal  open={isOpen} onClose={close}  >
-        <Box sx={{bgcolor : 'white' , position : "absolute" , top : "50%" , left : "50%"   , transform: 'translate(-50%, -50%)' , width : '400px' , height : "400px" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection: "column" , paddingX : "60px" , gap : "32px" }}  >
+        <Box sx={{bgcolor : 'white' , position : "absolute" , top : "50%" , left : "50%"   , transform: 'translate(-50%, -50%)' , width : '400px' , height : "600px" , display : "flex" , justifyContent : "center" , flexDirection: "column" , paddingX : "60px" , gap : "32px" }}  >
     <div className="flex flex-col  gap-4">
     <h3 className="text-black font-semibold text-3xl" >Create an Account</h3>
     <p className="text-xs" >By creating an account on Grailed you'll be able to buy, sell, comment, and more.</p>
     </div>
-    <div className="flex flex-col gap-3" >
+    <div className="flex flex-col w-full gap-3" >
         <FacebookSignInButton/>
         <AppleSignInButton/>
         <GoogleSignInButton />
 
     </div>
+    <div style={{borderTop: '0.01px solid #b1b3b1' }} className="w-full" ></div>
+    <div className="flex flex-col gap-4" >
+    <SecondaryButton onClick={openSignUpModal} className="font-bold" >CREATE ACCOUNT WITH EMAIL</SecondaryButton>
+    <p className="text-xs" >Already have an account? <span className="font-bold" >Log in</span></p>
+    </div>
+<p className="font-semibold text-center text-xs" >    By creating an account, I accept Grailed's <span className="font-bold" >Terms of Service</span>. For Grailed's<span className="font-bold" > Privacy Policy</span>, click here.</p>
 </Box>
 </Modal>
   );
--- src/modals/signUpModel.tsx
+++ src/modals/signUpModel.tsx
@@ -7,20 +7,24 @@ import { AppleSignInButton, FacebookSignInButton, GoogleSignInButton } from "@/c
 import { PrimaryInput } from "@/ui/input";
 import { useState } from "react";
 import { PrimaryButton } from "@/components/ui/buttons";
+import { SignUpFields, signUpFieldError } from "@/types/errors/auth";
+import { useSignUpModal } from "@/store/signUpModel";
 
 
 export default function LoginModal() {
-    const { isOpen , close , open}= useLoginModal()
+    const { isOpen , close , open}= useSignUpModal()
     const [password , setPassword ] = useState("")
     const [email , setEmail ] = useState("")
+    const  [fieldsErrors , setFieldsErrors ] = useState<signUpFieldError[]>([])
+
   return (
     <Modal  open={isOpen} onClose={close}  >
         <Box sx={{bgcolor : 'white' , position : "absolute" , top : "50%" , left : "50%"   , transform: 'translate(-50%, -50%)' , width : '400px' , height : "400px" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection: "column" , paddingX : "60px" , gap : "32px" }}  >
     <div className="flex flex-col  gap-4">
     <h3 className="text-black font-semibold text-3xl" >Create an Account</h3>
     </div>
-    <PrimaryInput value={email} onChange={(e)=>setEmail(e.target.value)} label="Email Address" />
-    <PrimaryInput value={password} onChange={(e)=>setPassword(e.target.value)} label="Password" />
+    <PrimaryInput error={fieldsErrors.find(item =>item.field === SignUpFields.EMAIL)?.message} value={email} onChange={(e)=>setEmail(e.target.value)} label="Email Address" />
+    <PrimaryInput error={fieldsErrors.find(item =>item.field === SignUpFields.PASSWORD)?.message} value={password} onChange={(e)=>setPassword(e.target.value)} label="Password" />
     <PrimaryButton >SIGN UP</PrimaryButton>
 </Box>
 </Modal>
