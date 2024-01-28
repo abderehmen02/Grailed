0482dc02eccb3b9ae0fcd02dd0191e090b28840e
abderrahmane <ilahalaha0987@gmail.com>
2024-01-28 22:17:11 +0100

    configuring the sign up errors

--- src/modals/signUpModel.tsx
+++ src/modals/signUpModel.tsx
@@ -1,26 +1,27 @@
 "use client"
 import { signIn } from "next-auth/react"; // or "next-auth/react"
-import { Modal } from "@mui/material";
+import { Checkbox, Modal } from "@mui/material";
 import { useLoginModal } from "@/store/loginModal";
 import {Box}from "@mui/material";
 import { AppleSignInButton, FacebookSignInButton, GoogleSignInButton } from "@/components/auth/components";
+import { PrimaryInput } from "@/ui/input";
+import { useState } from "react";
+import { PrimaryButton } from "@/components/ui/buttons";
 
 
 export default function LoginModal() {
     const { isOpen , close , open}= useLoginModal()
+    const [password , setPassword ] = useState("")
+    const [email , setEmail ] = useState("")
   return (
     <Modal  open={isOpen} onClose={close}  >
         <Box sx={{bgcolor : 'white' , position : "absolute" , top : "50%" , left : "50%"   , transform: 'translate(-50%, -50%)' , width : '400px' , height : "400px" , display : "flex" , alignItems : "center" , justifyContent : "center" , flexDirection: "column" , paddingX : "60px" , gap : "32px" }}  >
     <div className="flex flex-col  gap-4">
     <h3 className="text-black font-semibold text-3xl" >Create an Account</h3>
-    <p className="text-xs" >By creating an account on Grailed you'll be able to buy, sell, comment, and more.</p>
     </div>
-    <div className="flex flex-col gap-3" >
-        <FacebookSignInButton/>
-        <AppleSignInButton/>
-        <GoogleSignInButton />
-
-    </div>
+    <PrimaryInput value={email} onChange={(e)=>setEmail(e.target.value)} label="Email Address" />
+    <PrimaryInput value={password} onChange={(e)=>setPassword(e.target.value)} label="Password" />
+    <PrimaryButton >SIGN UP</PrimaryButton>
 </Box>
 </Modal>
   );
--- src/types/errors/auth.ts
+++ src/types/errors/auth.ts
@@ -1,18 +1,12 @@
 export enum SignUpFields {
-    USERNAME,
     PASSWORD,
-    FIRSTNAME,
-    LASTNAME,
-    GENDER,
-    BIRTHDATE,
     EMAIL,
+    PHONE_NUMBER ,
+    COUNTRY 
   }
 
 
-export type signUpZodErrorShortMessages = | "shortUsername" | "longUsername" | "shortPassword"
-  | "invalidBirthDate" | "invalidFirstName" | "shortFirstName"
-  | "longFirstName" | "invalidLastName" | "shortLastName"
-  | "longLastName" | "invalidEmail" | "missingProperty" | "invalidGender" | "requiredUsername" | "requiredPassword" | "requiredEmail" | "requiredFirstname" | "requiredLastname" | "requiredGender" |"requiredBirthDate" | "UsernameExists" | "EmailExists" | "EmailSignedInWithGoogle" ;
+export type signUpZodErrorShortMessages = "shortPassword"  |"invalidEmail" | "missingProperty" | "requiredPassword" | "requiredEmail"  | "EmailExists" | "EmailSignedInWithGoogle" ;
 
 
 type signUpFieldErrorsType   = {
@@ -27,29 +21,14 @@ type signUpFieldErrorsType   = {
 
 export const signUpZodErrors : signUpFieldErrorsType  =  {
     
-    shortUsername :{message : "Username must be at least 5 characters " , field : SignUpFields.USERNAME  ,  shortMessage : 'shortUsername' } ,
-    longUsername : { message :  "Username must be at most 50 characters" , field : SignUpFields.USERNAME, shortMessage : "longUsername"  } ,
     shortPassword: { message : "Password must be at least 8 charachters" , field : SignUpFields.PASSWORD, shortMessage : "shortPassword"  }    ,
-    invalidBirthDate :  { message :   "Birth date is not valid" , field : SignUpFields.BIRTHDATE,  shortMessage : "invalidBirthDate"  } ,
-    invalidFirstName : { message :  "Firstname is not valid" , field : SignUpFields.FIRSTNAME , shortMessage : "invalidFirstName" } ,
     missingProperty : {  message : "Some property is missing" , shortMessage : "missingProperty" }  ,
-    invalidLastName : { message :  "Lastname is not valid" , field : SignUpFields.LASTNAME , shortMessage : "invalidLastName"    }  ,
     invalidEmail :  { message :  "Email is not valid" , field : SignUpFields.EMAIL , shortMessage : "invalidEmail"  },
-    longFirstName : { message :  "Firstname must be less than 50 characters" , field: SignUpFields.FIRSTNAME , shortMessage : "longFirstName" }   ,
-    shortFirstName : {message  :  "Firstname must be more than 2 characters"  , field : SignUpFields.FIRSTNAME  , shortMessage : "shortFirstName"} ,
-    longLastName : {message   :"Lastname must be less 50 characters" ,field : SignUpFields.LASTNAME , shortMessage : "longLastName" } ,
-    shortLastName: {message : "Lastname must be more than 2 characters" , field : SignUpFields.LASTNAME , shortMessage : "shortLastName"  } ,
-    invalidGender : {message : "Gender is not valid" , field : SignUpFields.GENDER , shortMessage : "invalidGender" } ,
-    requiredBirthDate : {message : "Birthdate is required" , field: SignUpFields.BIRTHDATE , shortMessage : "requiredBirthDate"} ,
     requiredEmail : {message : "Email is required" , field : SignUpFields.EMAIL  , shortMessage : 'requiredEmail' } ,
-    requiredFirstname : {message : "Firstname is required" , field : SignUpFields.FIRSTNAME , shortMessage : "requiredFirstname" }  ,
-    requiredGender : {message : "Gender is required" , field : SignUpFields.GENDER , shortMessage :"requiredGender"} , 
-    requiredLastname : {message : "Lastname is required" , field : SignUpFields.LASTNAME , shortMessage : "requiredLastname"} ,
     requiredPassword : {message : "Password is required"   , field : SignUpFields.PASSWORD , shortMessage  :"requiredPassword"}  ,
-    requiredUsername : {message  : "Username is required" , field : SignUpFields.USERNAME , shortMessage : "requiredUsername"} ,
     EmailExists : {message : "Email already used! try another one" , field : SignUpFields.EMAIL , shortMessage  : "EmailExists" } ,
     EmailSignedInWithGoogle : {message : "Email already signed in with google! please try to sign in with google ussing this email!." , field : SignUpFields.EMAIL , shortMessage : "EmailSignedInWithGoogle" } ,
-    UsernameExists : { message : "Username already exists! try to use another username." ,field : SignUpFields.USERNAME , shortMessage : "UsernameExists" }
+    
 } 
 
 export type  signUpFieldError  =  {
@@ -62,11 +41,6 @@ export type  signUpFieldError  =  {
 
 
 
-export enum genderType {
-    male =  "MALE" ,
-    female ="FEMALE" ,
-    unknown = "UNKNOWN"
- } 
 
 
 export enum signUpErrorTypes {
