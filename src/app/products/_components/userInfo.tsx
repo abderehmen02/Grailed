32eb00e16327d7870342f3323ea7aef9f7d94561
abderrahmane <ilahalaha0987@gmail.com>
2024-01-30 19:13:16 +0100

    integrating the header in the products page

---
+++ src/app/products/_components/userInfo.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useQuery } from "@tanstack/react-query"
+
+export const UserInfo = ()=>{
+    // const {data , error , isLoading} = useQuery({
+    //     queryFn = async ()=>{
+    //         const data
+    //     }
+    // })
+    return <div className="flex items-center between pageMaxWidth" >
+        </div>
+}
