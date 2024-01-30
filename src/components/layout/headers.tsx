eafca85a5b222cd83d08cce870bd75599522d316
abderrahmane <ilahalaha0987@gmail.com>
2024-01-30 10:56:17 +0100

    fixing merging issues

--- src/components/layout/headers.tsx
+++ src/components/layout/headers.tsx
@@ -1,72 +1,14 @@
 
-import { appConfig } from "@/config/appConfig";
-import Link from "next/link";
-import { PrimaryButton, SecondaryButton, TextButton } from "../ui/buttons";
-import Image from "next/image";
-import NestedDropdown from "./NestedDropdown";
-
-export const Header = () => {
-  return (
-    <div>
-      <div className="flex border-whinGray border-b-2 items-center justify-center py-6 gap-40">
-        <div className="flex gap-4">
-          <video
-            poster="https://assets.grailed.com/logo.jpg"
-            className="w-[140px] h-auto"
-            autoPlay
-            loop
-            playsInline
-            muted
-          >
-            <source
-              src="https://assets.grailed.com/logo.webm"
-              type="video/webm"
-            />
-            <source
-              src="https://assets.grailed.com/logo.mp4"
-              type="video/mp4"
-            />
-          </video>
-          <div className="flex p-1 gap-5  centerContent  border-2 border-black">
-            <i
-              style={{ WebkitTextStroke: "1px" }}
-              className="bi bi-search  mx-4 h-fit  font-bold   "
-            ></i>
-            <input
-              placeholder="Search"
-              className="w-[400px] outline-none h-fit "
-            />
-            <TextButton className="border-stone-400 uppercase text-xs font-bold">
-              Search
-            </TextButton>
-          </div>
-        </div>
-        <div className="flex gap-3">
-          <PrimaryButton className="font-semibold">SELL</PrimaryButton>
-          <Link href={appConfig.routes.shop}>
-            <PrimaryButton className="border-none  font-semibold">
-              SHOP
-            </PrimaryButton>
-          </Link>
-          <Link href={appConfig.routes.drycleanonly}>
-            <PrimaryButton className="border-none font-semibold ">
-              READ
-            </PrimaryButton>
-          </Link>
-          <TextButton className="border-stone-500 font-semibold">
-            LOGIN
-          </TextButton>
-          <SecondaryButton>SITN UP</SecondaryButton>
-=======
 "use client"
 import { appConfig } from "@/config/appConfig"
 import Link from "next/link"
 import { PrimaryButton, SecondaryButton, TextButton } from "../ui/buttons"
-
+import Image from "next/image"
 import { useCookies } from 'next-client-cookies';
 import { authConfig } from "@/config/authConfig"
 import { useRouter } from "next/navigation"
 import { useCreateAccountModal } from "@/store/authModals";
+import NestedDropdown from "./NestedDropdown"
 
 export const Header  = ()=>{
     const cookies = useCookies()
@@ -597,5 +539,5 @@ export const Header  = ()=>{
         </div>
       </div>
     </div>
-  );
+  
 };
