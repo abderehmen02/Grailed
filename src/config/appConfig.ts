e902a048ed6217b60c2dcea6d3167a3ddb573a8a
abderrahmane <ilahalaha0987@gmail.com>
2024-01-31 18:21:07 +0100

    integrating the sell routes

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -6,7 +6,13 @@ export const appConfig = {
         categories : "/categories" ,
         collections : "/collections" ,
         sell : "/sell" ,
-        forSale : "/products"
+        forSale : "/sell/products" ,
+        sold : '/sell/sold' ,
+        drafts : '/sell/drafts' ,
+        feedback : '/sell/feedback' ,
+        vercationMode : '/sell/vacationMode' ,
+       payements : '/sell/payements' ,
+       settings : '/sell/settings'
     } ,
     url : "http://localhost:3000" ,
     unknownPersonImg : "/unknownPersonImg.png"
