b4774e8a454a59883d58f08903c27d0fcd34b10f
MahdiHasan943 <mahdifreelancer41@gmail.com>
2024-02-03 12:04:45 +0600

    added new card in home

--- src/app/(marketing)/_components/FeaturedCollections.tsx
+++ src/app/(marketing)/_components/FeaturedCollections.tsx
@@ -1,10 +1,67 @@
 import React from 'react'
 import Title from './Title'
+import Link from 'next/link'
 
 const FeaturedCollections = () => {
   return (
-      <div>
-          <Title text='Featured Collections & Stories'/>
+      <div className='py-8 px-4'>
+          <Title text='Featured Collections & Stories' />
+          
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+              <div className="relative  2xl:min-w-[750px]">
+                  <h3 className='absolute top-[40%] left-[30%] font-bold text-[#fff] text-[25px] sm:text-[40px]'>Knitwear Essentials</h3>
+                <img src="/images/The_New_Faces_of_Luxury-fb-desktop.jpg" className='w-full  h-[450px] ' alt="" />
+                  <div className="border py-8 px-4 items-center flex justify-between border-[#ddd]">
+                      <p className='text-[#000]'>CURATED COLLECTIONS</p>
+                  <Link href={'/shop'} className='uppercase text-[blue] border border-[blue] hover:text-[#fff] ease-linear font-bold  py-3 px-8  hover:bg-[blue] '>Shop + follow</Link>
+
+                 </div>
+              </div>
+
+              <div className="relative">
+                  <h3 className='absolute z-50 top-[40%] left-[30%] font-bold text-[#fff] text-[25px] sm:text-[40px]'>On-Location: PFW After Hours</h3>
+                  <div className="w-full  h-[450px] bg-[black] opacity-[0.9]">
+                      <img src="/images/fb-desktop__1_.jpg" className='w-full h-[450px] ' alt="" />
+                      </div>                  
+
+                  <div className="border py-8 px-4 items-center flex justify-between border-[#ddd]">
+                      <p className='text-[#000]'>STREET STYLE
+
+</p>
+                  <Link href={'/shop'} className='uppercase text-[blue] border border-[blue] hover:text-[#fff] ease-linear font-bold  py-3 px-8  hover:bg-[blue] '>READ MORE</Link>
+
+                 </div>
+              </div>
+
+
+              <div className="relative">
+                  <h3 className='absolute z-50 top-[40%] opacity-[1] left-[30%] font-bold text-[#fff] text-[25px] sm:text-[40px]'>Everything Vintage</h3>
+                  <div className="w-full  h-[450px] bg-[black] opacity-[0.8]">
+                  <img src="/images/vintage-denim-fb-desktop.jpg" className='w-full h-[450px] ' alt="" />
+
+                  </div>                  
+                  <div className="border py-8 px-4 items-center flex justify-between border-[#ddd]">
+                      <p className='text-[#000]'>CURATED COLLECTIONS</p>
+                  <Link href={'/shop'} className='uppercase text-[blue] border border-[blue] hover:text-[#fff] ease-linear font-bold  py-3 px-8  hover:bg-[blue] '>Shop + follow</Link>
+
+                 </div>
+              </div>
+
+
+
+              <div className="relative">
+                  <h3 className='absolute top-[40%] left-[30%] font-bold text-[#fff] text-[25px] sm:text-[40px]'>Post-Sneaker World</h3>
+                <img src="/images/fb-desktop.jpg" className='w-full h-[450px] ' alt="" />
+                  <div className="border py-8 px-4 items-center flex justify-between border-[#ddd]">
+                      <p className='text-[#000]'>CURATED COLLECTIONS</p>
+                  <Link href={'/shop'} className='uppercase text-[blue] border border-[blue] hover:text-[#fff] ease-linear font-bold  py-3 px-8  hover:bg-[blue] '>Shop + follow</Link>
+
+                 </div>
+              </div>
+
+
+
+          </div>
     </div>
   )
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,16 @@
+import Footer from '@/components/layout/footer'
+import { Header } from '@/components/layout/headers'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+      <>
+          <Header />
+          <div className="px-4 sm:px-16 min-h-[400px] py-8">
+              shop all
+          </div>
+          <Footer/>
+    </>
   )
 }
 
